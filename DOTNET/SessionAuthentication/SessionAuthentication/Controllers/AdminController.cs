f3d230a80ab5db4ec38ddcbaea31fd3857deca08
owaisk23 <owais1566@gmail.com>
2026-05-02 19:55:09 +0500

    session auth

---
+++ DOTNET/SessionAuthentication/SessionAuthentication/Controllers/AdminController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SessionAuthentication.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return View();
+            if (HttpContext.Session.GetString("role") == "admin")
+            {
+
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string pwd)
+        {
+            if (email == "admin@gmail.com" && pwd == "123")
+            {
+                HttpContext.Session.SetString("role", "admin");
+                HttpContext.Session.SetString("email", email);
+                return RedirectToAction("Index");
+
+            }
+            else if (email == "user@gmail.com" && pwd == "123")
+            {
+                HttpContext.Session.SetString("role", "user");
+                HttpContext.Session.SetString("email", email);
+                return RedirectToAction("Index", "Home");
+            }
+            else if (email == "owais@gmail.com" && pwd == "123")
+            {
+                HttpContext.Session.SetString("role", "client");
+                HttpContext.Session.SetString("email", email);
+                return RedirectToAction("Index", "Client");
+            }
+            else
+            {
+                ViewBag.msg = "Invalid credentials";
+                return View();
+            }
+        }
+
+
+        public IActionResult AdminLogout()
+        {
+            HttpContext.Session.Clear();
+            HttpContext.Session.Remove("role");
+            HttpContext.Session.Remove("email");
+            return RedirectToAction("Login");
+
+        }
+    }
+}
