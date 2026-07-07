d5849a7b1686c4a3d8c689e2234f9dc91fa9a3fd
owaisk23 <owais1566@gmail.com>
2026-07-07 20:49:15 +0500

    added basics of ts

--- Angular/Typescript/app.js
+++ Angular/Typescript/app.js
@@ -1,1 +1,43 @@
 console.log("Welcome in Typescript");
+var userName = "Owais Ahmed Khan";
+// console.log("User Name: " + userName);
+var userAge = 25;
+// console.log("User Age: " + userAge);
+var userEmail = "owais_ahmed@aptechnorth.edu.pk";
+// console.log("User Email: " + userEmail);
+// let userDetails = `Name: ${userName}, Age: ${userAge}, Email: ${userEmail}`;
+// console.log(userDetails);
+// Null= false       True=1 False=0
+var userId = null;
+if (userId) {
+    console.log("User Id is true");
+}
+else {
+    console.log("User Id is false");
+}
+// undefined= false       True=1 False=0
+var userPost = undefined;
+if (userPost) {
+    console.log("User Post is true");
+}
+else {
+    console.log("User Post is false");
+}
+//ARRAYS
+var userNames = ["Hamza", "Haris", "Abdullah", "Anas"];
+console.log("User Names: " + userNames);
+//map method
+var userNamesMap = userNames.map(function (val) {
+    return val.toLowerCase();
+});
+//forEach method
+userNames.forEach(function (val) {
+    console.log("User Name: " + val);
+});
+console.log("User Names Map: " + userNamesMap);
+var userDetails = {
+    name: "Owais Ahmed Khan",
+    age: 25,
+    email: "owais_ahmed@aptechnorth.edu.pk"
+};
+console.log(userDetails);
--- Angular/Typescript/app.ts
+++ Angular/Typescript/app.ts
@@ -1,1 +1,58 @@
 console.log("Welcome in Typescript");
+
+let userName: string = "Owais Ahmed Khan";
+// console.log("User Name: " + userName);
+
+let userAge: number = 25;
+// console.log("User Age: " + userAge);
+
+let userEmail: string = "owais_ahmed@aptechnorth.edu.pk";
+// console.log("User Email: " + userEmail);
+
+// let userDetails = `Name: ${userName}, Age: ${userAge}, Email: ${userEmail}`;
+// console.log(userDetails);
+
+// Null= false       True=1 False=0
+
+let userId = null;
+
+if(userId) {
+    console.log("User Id is true");
+} else {
+    console.log("User Id is false");
+}
+
+// undefined= false       True=1 False=0
+
+let userPost = undefined;
+
+if(userPost) {
+    console.log("User Post is true");
+} else {
+    console.log("User Post is false");
+}
+
+//ARRAYS
+
+let userNames: string[] = ["Hamza", "Haris", "Abdullah", "Anas"];
+console.log("User Names: " + userNames);
+
+//map method
+let userNamesMap = userNames.map((val) => {
+    return val.toLowerCase();
+});
+
+//forEach method
+userNames.forEach((val) => {
+    console.log("User Name: " + val);
+});
+
+console.log("User Names Map: " + userNamesMap);
+
+let userDetails: any = {
+    name: "Owais Ahmed Khan",
+    age: 25,
+    email: "owais_ahmed@aptechnorth.edu.pk"
+};
+
+console.log(userDetails)
