5a5227f57ade46a8a46025a360422d73593ee342
owaisk23 <owais1566@gmail.com>
2026-08-01 19:48:02 +0500

    update

--- Angular/Typescript/app.js
+++ Angular/Typescript/app.js
@@ -1,8 +1,21 @@
 console.log("Welcome in Typescript");
-var stdDetails = {
-    name: "Hamza",
-    age: 22,
-    isActive: true
-};
-console.log("Student Details: " + stdDetails.name + ", " + stdDetails.age + ", " + stdDetails.isActive);
-console.log(stdDetails["age"]);
+var staff = [
+    [0, "Hassan", "hassan@gmail.com", "CAH"],
+    [1, "Ali", "ali@gmail.com"],
+    [2, "Saad", "saad@gmail.com"],
+    [3, "Owais", "owais@gmail.com", "Manager Academics"]
+];
+var payStubs = [
+    [staff[0], 300],
+    [staff[1], 250, 300],
+    [staff[2], 300, 400, 250],
+];
+// const monthOnePayments = payStubs[0][1] + payStubs[1][1] + payStubs[2][1]
+//300       +   250          +  300   
+// const monthTwoPayments = payStubs[1][2] + payStubs[2][2];
+//                         //300         +   400
+var monthThreePayments = payStubs[2][3];
+//
+// console.log(monthOnePayments)
+// console.log(monthTwoPayments)
+console.log(monthThreePayments);
--- Angular/Typescript/app.ts
+++ Angular/Typescript/app.ts
@@ -85,22 +85,128 @@ console.log("Welcome in Typescript");
 //     }
 // });
 
-//ALIASED object type
+// //ALIASED object type
 
-type Std = {
-    name: string,
-    age: number,
-    isActive: boolean
-}
+// type Std = {
+//     name: string,
+//     age: number,
+//     isActive: boolean
+// }
 
 
-let stdDetails: Std = {
-    name: "Hamza",
-    age: 22,
-    isActive: true
-}
+// let stdDetails: Std = {
+//     name: "Hamza",
+//     age: 22,
+//     isActive: true
+// }
 
-console.log("Student Details: " + stdDetails.name + ", " + stdDetails.age + ", " + stdDetails.isActive);
-console.log(stdDetails["age"]);
+// console.log("Student Details: " + stdDetails.name + ", " + stdDetails.age + ", " + stdDetails.isActive);
+// console.log(stdDetails["age"]);
+
+//Interfaces
+
+// interface Manager {
+//     name: string;
+//     salary?: number;
+// }
+
+// let storeManager: Manager = {
+//     name: "Haris Naseer",
+//     salary: 50000
+// }
+
+// // Intersection Types
+// interface Teacher {
+//     teacherId: number,
+//     teacherName: string
+// }
+
+// interface Student {
+//     stdId: number,
+//     stdName: string
+// }
+
+// type intersectionType = Teacher & Student;
+
+// let intersectionObj: intersectionType = {
+//     teacherId: 1,
+//     teacherName: "Owais Ahmed Khan",
+//     stdId: 1,
+//     stdName: "Jaffery"
+// }
+
+// console.log("Intersection Object: " + intersectionObj.teacherName + ", " + intersectionObj.stdName);
+
+
+
+
+// enum Color {Red, Green, Blue};
+
+// var c: Color = Color.Blue;
+// console.log(c)
+
+// enum Color1 {Red = 5, Green, Blue}
+// // //              // 0       1      2 
+// //             // 1        2       3
+// var colorName: string = Color1[7]
+// console.log(colorName)
+
+// // // assigning number of your choice to enum
+// enum Color2 {Red = 2, Green = 4, Blue = 7, Cyan = 8}
+
+// var colorName: string = Color2[7]
+// console.log(colorName)   
+
+// Tuple types have advantages that you can accurately describe the type of an array
+
+// var tuple: [number, string] = [1, "Bob"];
+// // var scndElement = tuple[1]
+// // console.log(scndElement)
+// // console.log(tuple[1])
+
+// In JS
+// const failingResponse = ["Non Found", 404]
+// //  In TS
+// const passingResponse: [string, number] = ["{}", 200]
+
+// if(passingResponse[1] === 200){
+//     const localInfo = JSON.parse(passingResponse[0])
+//     console.log(localInfo)
+// }
+
+
+type staffAccount = [number, string, string, string?]
+
+const staff: staffAccount[] = [
+    [0, "Hassan", "hassan@gmail.com", "CAH"],
+    [1, "Ali", "ali@gmail.com"],
+    [2, "Saad", "saad@gmail.com"],
+    [3, "Owais", "owais@gmail.com", "Manager Academics"]
+];
+
+type PaySTubs = [staffAccount, ...number[]];
+
+const payStubs: PaySTubs[] = [
+    [staff[0], 300],
+    [staff[1], 250, 300],
+    [staff[2], 300, 400, 250],
+];
+
+// const monthOnePayments = payStubs[0][1] + payStubs[1][1] + payStubs[2][1]
+                            //300       +   250          +  300   
+// const monthTwoPayments = payStubs[1][2] + payStubs[2][2];
+//                         //300         +   400
+const monthThreePayments = payStubs[2][3];
+//
+
+// console.log(monthOnePayments)
+// console.log(monthTwoPayments)
+console.log(monthThreePayments)
+
+
+
+
+
+
 
 
