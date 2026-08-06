614a14e215def411a2f46dd0ba32edd0f8380990
owaisk23 <owais1566@gmail.com>
2026-08-06 20:47:49 +0500

    added delete and get by id

--- WebAPI/Backend/Backend/Controllers/MyController.cs
+++ WebAPI/Backend/Backend/Controllers/MyController.cs
@@ -5,7 +5,7 @@ namespace Backend.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    
+
     public class MyController : Controller
     {
         private readonly BikeContext db;
@@ -20,11 +20,35 @@ namespace Backend.Controllers
         //{
         //    return Ok("OWAIS AHMED KHAN");
         //}
-        
+
         [HttpGet]
         public IActionResult GetBikes()
         {
             return Ok(db.Bikes.ToList());
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBikes(int id)
+        {
+            return Ok(db.Bikes.Find(id));
+        }
+
+        [HttpPost]
+        public IActionResult AddBikes(Bike bike)
+        {
+            db.Bikes.Add(bike);
+            db.SaveChanges();
+            return StatusCode(201);
+        }
+        
+        [HttpDelete]
+        public IActionResult DeleteBike(int id)
+        {
+            var bike = db.Bikes.Find(id);
+            db.Bikes.Remove(bike);
+            db.SaveChanges();
+            return StatusCode(201);
+        }
+
     }
 }
--- WebAPI/Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ WebAPI/Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a5227f57ade46a8a46025a360422d73593ee342")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c272df331464c0a6daed603254f27b7a4274b6f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
