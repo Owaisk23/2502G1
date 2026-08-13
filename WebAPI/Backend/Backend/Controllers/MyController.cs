efa4f46b0ad0e2f28eba7e59c3bdc5c6a6348bec
owaisk23 <owais1566@gmail.com>
2026-08-13 19:13:19 +0500

    added search and pagination

--- WebAPI/Backend/Backend/Controllers/MyController.cs
+++ WebAPI/Backend/Backend/Controllers/MyController.cs
@@ -50,5 +50,51 @@ namespace Backend.Controllers
             return StatusCode(201);
         }
 
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBike(int id, Bike updateBike)
+        {
+            var bike = db.Bikes.Find(id);
+            bike.Name = updateBike.Name;
+            bike.Brand = updateBike.Brand;
+            bike.Price = updateBike.Price;
+            db.Bikes.Update(bike);
+            db.SaveChanges();
+            return StatusCode(201);
+        }
+
+        [HttpGet("Search/{query}")]
+        public IActionResult SearchProduct(string query)
+        {
+            // For partial match search
+            var products = db.Bikes
+                                   .Where(o => o.Name.Contains(query)
+                                           || o.Brand.Contains(query)
+                                          )
+                                   .ToList(); // Partial Match
+
+            return Ok(products);
+        }
+
+        [HttpGet("Pagination/{pageNo}/{pageSize}")]
+        public IActionResult Pagination(int pageNo = 1, int pageSize = 2)
+        {
+            int pageno = pageNo;
+            if (pageno < 1) pageno = 1;
+            int pagesize = pageSize;
+            if (pagesize < 1) pagesize = 1;
+
+            var getEvents = db.Bikes.Skip((pageno - 1) * pagesize).Take(pagesize);
+
+            if (getEvents != null)
+            {
+                return Ok(getEvents);
+            }
+            else
+            {
+                return Ok("End of list");
+            }
+
+        }
     }
 }
--- WebAPI/Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ WebAPI/Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c272df331464c0a6daed603254f27b7a4274b6f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+614a14e215def411a2f46dd0ba32edd0f8380990")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
